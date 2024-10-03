ff04b7ff1aa7ec3d4ed51e738b8a0ded89e0430f
Yasiru Swaris <yasirus@99x.io>
2024-10-03 21:56:34 +0530

    03/10/2024 (22) eve

---
+++ API/CultureIntelligence.API/CultureIntelligence.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CultureIntelligence.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CultureIntelligence.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
